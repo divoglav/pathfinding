0dba2d75a00685c5eb12c69ea65f6faca75cba7b
divoglav <divoglav@protonmail.com>
2024-07-26 14:08:24 +0300

    refactor: display

--- src/display.ts
+++ src/display.ts
@@ -2,16 +2,12 @@ import config from "./config";
 import { Cell } from "./cell";
 
 export class Display {
-  private _context: CanvasRenderingContext2D;
+  private readonly _cellDisplayWidth = config.canvas.width / config.grid.rows;
+  private readonly _cellDisplayHeight = config.canvas.height / config.grid.columns;
+  private readonly _cellAdjustedWidth = this._cellDisplayHeight - config.display.border;
+  private readonly _cellAdjustedHeight = this._cellDisplayHeight - config.display.border;
 
-  private _cellDisplayWidth = config.canvas.width / config.grid.rows;
-  private _cellDisplayHeight = config.canvas.height / config.grid.columns;
-  private _cellAdjustedWidth = this._cellDisplayHeight - config.display.border;
-  private _cellAdjustedHeight = this._cellDisplayHeight - config.display.border;
-
-  constructor(context: CanvasRenderingContext2D) {
-    this._context = context;
-  }
+  constructor(private readonly _context: CanvasRenderingContext2D) {}
 
   displayCell(x: number, y: number) {
     this._context.fillRect(
@@ -28,49 +24,19 @@ export class Display {
 
     // Separating different color batches for performance
 
-    this._context.fillStyle = config.display.colors.empty;
-    for (let x = 0; x < rows; x++) {
-      for (let y = 0; y < cols; y++) {
-        if (cells[x][y].hasState(Cell.EMPTY)) {
-          this.displayCell(x, y);
+    const drawCellsBatch = (cellState: number, color: string) => {
+      this._context.fillStyle = color;
+      for (let x = 0; x < rows; x++) {
+        for (let y = 0; y < cols; y++) {
+          if (cells[x][y].hasState(cellState)) this.displayCell(x, y);
         }
       }
-    }
+    };
 
-    this._context.fillStyle = config.display.colors.block;
-    for (let x = 0; x < rows; x++) {
-      for (let y = 0; y < cols; y++) {
-        if (cells[x][y].hasState(Cell.BLOCK)) {
-          this.displayCell(x, y);
-        }
-      }
-    }
-
-    this._context.fillStyle = config.display.colors.open;
-    for (let x = 0; x < rows; x++) {
-      for (let y = 0; y < cols; y++) {
-        if (cells[x][y].hasState(Cell.OPEN)) {
-          this.displayCell(x, y);
-        }
-      }
-    }
-
-    this._context.fillStyle = config.display.colors.closed;
-    for (let x = 0; x < rows; x++) {
-      for (let y = 0; y < cols; y++) {
-        if (cells[x][y].hasState(Cell.CLOSED)) {
-          this.displayCell(x, y);
-        }
-      }
-    }
-
-    this._context.fillStyle = config.display.colors.path;
-    for (let x = 0; x < rows; x++) {
-      for (let y = 0; y < cols; y++) {
-        if (cells[x][y].hasState(Cell.PATH)) {
-          this.displayCell(x, y);
-        }
-      }
-    }
+    drawCellsBatch(Cell.EMPTY, config.display.colors.empty);
+    drawCellsBatch(Cell.BLOCK, config.display.colors.block);
+    drawCellsBatch(Cell.OPEN, config.display.colors.open);
+    drawCellsBatch(Cell.CLOSED, config.display.colors.closed);
+    drawCellsBatch(Cell.PATH, config.display.colors.path);
   }
 }
